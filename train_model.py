81665071ef6e9791dad9e87f973ae56ab81283d0
idonorchidinma <idonor.chidinma@gmail.com>
2025-08-13 18:26:25 -0500

    initial commit

---
+++ train_model.py
@@ -0,0 +1,49 @@
+
+
+
+import joblib
+from sklearn.linear_model import LinearRegression
+from preprocessing import load_data, preprocess
+
+def train():
+   
+    df = load_data("data/housing.csv")
+
+    
+    df = preprocess(df)
+
+   
+    X = df.drop(columns=["price"])
+    y = df["price"]
+
+ 
+    model = LinearRegression()
+    model.fit(X, y)
+
+   
+    joblib.dump(model, "model.pkl")
+    print("Model trained and saved as model.pkl")
+
+if __name__ == "__main__":
+    train()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
